d53773c9bb1c3a555080910286759a6fa70773ff
VentGrey <omarpurataf@gmail.com>
2020-03-09 17:41:13 -0600

    New main explaining open()

--- io.c
+++ io.c
@@ -8,5 +8,22 @@
  * file is opened by the given flags
  */
 
-int open(const char *name, int flags);
-int open(const char *name, int flags, mode_t mode);
+// int open(const char *name, int flags);
+// int open(const char *name, int flags, mode_t mode);
+
+
+int main(int argc, char *argv[]) {
+
+        int fd;
+
+        /* The following code opens a Gentoo ebuild in Read Only */
+        fd = open("/home/omar/Escritorio/flora/app-admin/equo/equo-319.ebuild",
+                  O_RDONLY);
+
+        // In case fd fails to open the file return 1 instead
+        if (fd == -1)
+                return 1;
+
+        return 0;
+}
+
